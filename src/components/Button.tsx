943330fbb4b89989ebf997a2f65fe6e14895ee54
Blagoj Petrov <blagoj.petrov5@gmail.com>
2022-12-17 12:24:22 +0100

    Code improvments

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,20 @@
+import clsx from "clsx";
+
 /* eslint-disable react/react-in-jsx-scope */
 type ButtonProps = {
   disabled?: boolean;
   onClick: () => void;
   children: string;
+  className?: string;
 };
 
-const Button = ({ disabled, onClick, children }: ButtonProps) => {
+const Button = ({ disabled, onClick, children, className }: ButtonProps) => {
   return (
     <button
-      className="ml-auto p-4 block bg-primary rounded-lg disabled:opacity-80 disabled:cursor-not-allowed"
+      className={clsx(
+        "ml-auto p-4 block bg-primary rounded-lg disabled:opacity-80 disabled:cursor-not-allowed",
+        className
+      )}
       disabled={disabled}
       onClick={onClick}
     >
--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import React from "react";
 
 type Props = { children: string | string[]; className?: string };
@@ -6,10 +7,10 @@ export const Title = ({ children }: Props) => (
   <h1 className="text-gray-200 text-2xl mb-4">{children}</h1>
 );
 
-export const Subtitle = ({ children }: Props) => (
-  <h2 className="text-gray-200 text-xl mb-4">{children}</h2>
+export const Subtitle = ({ children, className }: Props) => (
+  <h2 className={clsx("text-gray-200 text-xl mb-4", className)}>{children}</h2>
 );
 
-export const Text = ({ children, className = "" }: Props) => (
-  <p className={`text-gray-200 text-lg ${className}`}>{children}</p>
+export const Text = ({ children, className }: Props) => (
+  <p className={clsx("text-gray-200 text-lg", className)}>{children}</p>
 );
